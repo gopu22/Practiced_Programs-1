0cba28b6d67b2988c2179d18cce7996a1f717f92
Govardhan Reddy B <govardhanbs2000@gmail.com>
2023-07-18 03:13:52 +0530

    18-Jul-2023

---
+++ enumerate_indexes.py
@@ -0,0 +1,7 @@
+#This program prints the index numbers of a given list
+
+l=[1,2,3,4]  # The list is stored in variable 'l'
+for index, item in enumerate(l):  # The for loop taking index & item from enumerate(l)
+    print(index, item)  # This line prints the elements in item along with their index numbers
+
+
