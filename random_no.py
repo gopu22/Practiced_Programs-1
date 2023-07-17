83dc71966932aaf00e5dbfc79a548103e766be46
Govardhan Reddy B <govardhanbs2000@gmail.com>
2023-07-17 22:22:17 +0530

    first commit

---
+++ random_no.py
@@ -0,0 +1,7 @@
+#----------------- Program to Print Random Numbers--------------
+
+from random import *   # importing random module
+for i in range(10):    # for loop in range of 10
+    print(randint(0,9),randint(0,9),randint(0,9),randint(0,9),randint(0,9),randint(0,9), sep="")   # printing random integers between 0-9
+
+
