1a4c03a5003d6ab9e5129e5c24fea3898f050939
Govardhan Reddy B <govardhanbs2000@gmail.com>
2023-07-18 23:52:02 +0530

    18-Jul-2023

---
+++ basic_whileloop.py
@@ -0,0 +1,9 @@
+# Program that prints 'your name' when user input is equal to the defined 'your name'
+
+name=''  # Storing empty string in a variable called "name"
+while name!="your name":  # While loop becomes true when name=="your name"
+    print('please type your name')  # Asking user to enter the name
+    name=input()  # Storing user input in the name variable
+print('thank you')  # This line gets executed when the while loop becomes true
+
+
