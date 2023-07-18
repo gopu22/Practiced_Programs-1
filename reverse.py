6e5aad34b7641103415a23f2e12f941c389fca8c
Govardhan Reddy B <govardhanbs2000@gmail.com>
2023-07-18 23:44:35 +0530

    18-Jul-2023

---
+++ reverse.py
@@ -0,0 +1,6 @@
+# program to print the given input in reverse order.
+
+string=input("enter the number: ")  # Takes user input here
+reverse=string[::-1]  # Using slicing method to reverse the user input
+print(reverse)  # Printing  the user input in reverse method
+
