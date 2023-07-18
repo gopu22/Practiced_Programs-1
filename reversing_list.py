14d205a87c07d7045acd696f57abf0c03cc9c9da
Govardhan Reddy B <govardhanbs2000@gmail.com>
2023-07-18 23:57:44 +0530

    18-Jul-2023

---
+++ reversing_list.py
@@ -0,0 +1,10 @@
+# Program to reverse the given two lists into single list.
+
+a=[1,16,22,4,30]  # List 'a'
+b=[6,56,8,12,10]  # List 'b'
+add=a+b  # Adding the two lists 'a' & 'b'
+add.sort()  # Sorting the added list
+reverse=add[::-1]  # Reversing the added list
+print(reverse)  # Printing the reversed list
+#OUTPUT
+
