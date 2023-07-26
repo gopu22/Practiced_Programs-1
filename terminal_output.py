f55a45303638dcee1b326a774e059932807cd833
Govardhan Reddy B <govardhanbs2000@gmail.com>
2023-07-26 21:55:16 +0530

    26-Jul-2023

---
+++ terminal_output.py
@@ -0,0 +1,5 @@
+# prints the values that is given along with the python running command
+
+import sys  # importing system modules
+value=sys.argv[1]  # storing argument 1 in value
+print(value)  # printing value
