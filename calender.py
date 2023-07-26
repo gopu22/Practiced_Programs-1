0465eb5095f95a757523cf551ef5229ab5176366
Govardhan Reddy B <govardhanbs2000@gmail.com>
2023-07-26 22:23:40 +0530

    26-Jul-2023

---
+++ calender.py
@@ -0,0 +1,6 @@
+# program to print the calender
+
+import calendar  # import module called 'calender'
+mm=4  # specify the month
+yy=2000  # specify the year
+print(calendar.month(yy,mm))  # print the output
