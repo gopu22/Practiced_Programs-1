1fb6cfa1eb742f663886c7e304cb87b830c02d1f
Govardhan Reddy B <govardhanbs2000@gmail.com>
2023-07-26 22:27:59 +0530

    26-Jul-2023

---
+++ n*n_matrix.py
@@ -0,0 +1,6 @@
+# program to print the n*n matrix
+
+n=int(input("Enter the number: "))  # user input
+for i in range(n):  # for loop with range of 'n'
+    print("* "*n)  # printing the output
+print()
