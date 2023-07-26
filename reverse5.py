af311a513a4b1197eff40a7fb35086a962d4d4a7
Govardhan Reddy B <govardhanbs2000@gmail.com>
2023-07-27 01:33:57 +0530

    27-Jul-2023

---
+++ reverse5.py
@@ -0,0 +1,5 @@
+# program to print characters at even & odd positions of a string using a slice() operator
+
+s=input("enter the string: ")  # user input
+print("it is even string: ",s[::2])
+print("it is even string: ",s[1::2])
