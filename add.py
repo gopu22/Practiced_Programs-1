6e5b410e85fd5be9c81e12599333c533c030957b
Govardhan Reddy B <69630416+gopu22@users.noreply.github.com>
2023-08-18 23:46:27 +0530

    Update add.py

--- add.py
+++ add.py
@@ -1,7 +1,7 @@
 a=300 #value 'a'
 b=100 #value 'b'
-print('The sum is:',a+b)
-print('The sub is:',a-b)
-print('The mul is:',a*b)
-print('The sub is:',a/b)
-print('The sub is:',a%b)
+print('The sum is:',a+b) #output
+print('The sub is:',a-b) #output
+print('The mul is:',a*b) #output
+print('The sub is:',a/b) #output
+print('The sub is:',a%b) #output
